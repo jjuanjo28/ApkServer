4f73a8b192a928696671d2f9110ed508f18cb52a
jjuanjo28 <105110146+jjuanjo28@users.noreply.github.com>
2023-10-30 10:54:32 -0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -16,19 +16,6 @@ const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
   port: DB_PORT,
-    pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
  });
-
-try {
-    await db.authenticate()
-    console.log("coneccion exitosa a DB")
-} catch (error) {
-    console.log(`el error de coneccion es: ${error}`)
-}
 
 export default db;
