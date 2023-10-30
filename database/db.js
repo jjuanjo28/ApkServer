4105f8d8826a0d1ca01b3c656a5be0fe81485aa2
jjuanjo28 <105110146+jjuanjo28@users.noreply.github.com>
2023-10-30 10:06:51 -0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -7,6 +7,11 @@ import {
   DB_PORT
 } from "../src/config.js"
 
+console.log("DB_HOST:", DB_HOST)
+console.log("DB_NAME:", DB_NAME)
+console.log("DB_USER:", DB_USER)
+console.log("DB_PORT:", DB_PORT)
+
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
